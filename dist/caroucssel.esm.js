61be27e5296f7fd2b6d839f98ec58294b4920f28
Norman Rusch <norman.rusch@moccu.com>
2019-02-11 14:29:25 +0100

    Create dist

---
+++ dist/caroucssel.esm.js
@@ -0,0 +1,300 @@
+class Scrollbar {
+
+	constructor() {
+		this._d = null;
+		window.addEventListener('resize', () => this._d = null);
+	}
+
+	get dimensions() {
+		this._d = this._d || (() => {
+			const
+				inner = document.createElement('div'),
+				outer = document.createElement('div')
+			;
+			let
+				width, w1, w2,
+				height, h1, h2
+			;
+
+			document.body.appendChild(outer);
+			outer.style.position = 'absolute';
+			outer.style.top = '0px';
+			outer.style.left = '0px';
+			outer.style.visibility = 'hidden';
+			outer.appendChild(inner);
+
+			// Calculate width:
+			inner.style.width = '100%';
+			inner.style.height = '200px';
+			outer.style.width = '200px';
+			outer.style.height = '150px';
+			outer.style.overflow = 'hidden';
+			w1 = inner.offsetWidth;
+			outer.style.overflow = 'scroll';
+			w2 = inner.offsetWidth;
+			w2 = (w1 === w2) ? outer.clientWidth : w2;
+			width = w1 - w2;
+
+			// Calculate height:
+			inner.style.width = '200px';
+			inner.style.height = '100%';
+			outer.style.width = '150px';
+			outer.style.height = '200px';
+			outer.style.overflow = 'hidden';
+			h1 = inner.offsetHeight;
+			outer.style.overflow = 'scroll';
+			h2 = inner.offsetHeight;
+			h2 = (h1 === h2) ? outer.clientHeight : h2;
+			height = h1 - h2;
+
+			document.body.removeChild(outer);
+			return {width, height};
+		})();
+
+		return this._d;
+	}
+
+}
+
+function __render(template, data) {
+	const el = document.createElement('div');
+	el.innerHTML = template(data);
+	return el.firstChild;
+}
+
+
+function __templateButton({className, controls, label, title}) {
+	return `<button class="${className}" aria-label="${label}" title="${title}" aria-controls="${controls}">
+		<span>${label}</span>
+	</button>`;
+}
+
+
+function __templatePagination({className, controls, items, label, title}) {
+	return `<ul class="${className}">
+		${items.map((item, index) => {
+			const data = {index, item, items};
+			const labelStr = label(data);
+			const titleStr = title(data);
+			return `<li>
+				<button aria-controls="${controls}" aria-label="${titleStr}" title="${titleStr}">
+					<span>${labelStr}</span>
+				</button>
+			</li>`;
+		}).join('')}
+	</ul>`;
+}
+
+
+const
+	ID_NAME = (count) => `caroucssel-${count}`,
+	ID_MATCH = /^caroucssel-[0-9]*$/,
+	CLASS_VISIBLE_SCROLLBAR = 'has-visible-scrollbar',
+	CLASS_INVISIBLE_SCROLLBAR = 'has-invisible-scrollbar',
+
+	DEFAULTS = {
+		// Buttons:
+		hasButtons: false,
+		buttonClassName: 'button',
+		buttonTemplate: __templateButton,
+		buttonPrevious: null,
+		buttonNext: null,
+
+		// Pagination:
+		hasPagination: false,
+		paginationClassName: 'pagination',
+		paginationLabel: ({index}) => `${index + 1}`,
+		paginationTitle: ({index}) => `Go to ${index + 1}. item`,
+		paginationTemplate: __templatePagination
+	},
+	DEFAULTS_BUTTON_PREVIOUS = {
+		className: 'is-previous',
+		label: 'Previous',
+		title: 'Go to previous'
+	},
+	DEFAULTS_BUTTON_NEXT = {
+		className: 'is-next',
+		label: 'Next',
+		title: 'Go to next'
+	}
+;
+
+
+let
+	carouselCount = 0,
+	scrollbar = null
+;
+
+
+class Carousel {
+
+	constructor(el, options = {}) {
+		if (!el || !(el instanceof HTMLElement)) {
+			throw new Error(`Carousel needs a dom element but "${(typeof el)}" was passed.`);
+		}
+
+		this._el = el;
+
+		// Create a singleton instance of scrollbar for all carousel instances:
+		scrollbar = scrollbar || new Scrollbar();
+
+		// Count all created instances to create unique id, if given dom element
+		// has no id-attribute:
+		carouselCount++;
+		el.id = el.id || ID_NAME(carouselCount);
+		this._id = el.id;
+
+		// "deep" extend options and defaults:
+		this._options = {...DEFAULTS, ...options};
+		this._options.buttonPrevious = {...DEFAULTS_BUTTON_PREVIOUS, ...options.buttonPrevious};
+		this._options.buttonNext = {...DEFAULTS_BUTTON_NEXT, ...options.buttonNext};
+
+		// Render
+		this._update();
+		this._addButtons();
+		this._addPagination();
+	}
+
+	get el() {
+		return this._el;
+	}
+
+	get id() {
+		return this._id;
+	}
+
+	get index() {
+		const {el} = this;
+		const {children, clientWidth} = el;
+		const {length} = children;
+		const outerLeft = el.getClientRects()[0].left;
+		const offset = clientWidth / 2;
+
+		let
+			index = 0,
+			left
+		;
+
+		for (;index < length; index++) {
+			left = el.children[index].getClientRects()[0].left - outerLeft + offset;
+			if (left >= 0 && left < clientWidth) {
+				return index;
+			}
+		}
+
+		return index - 1;
+	}
+
+	set index(value) {
+		const {el} = this;
+		const {children, scrollLeft} = el;
+		const from = {scrollLeft};
+
+		if (-1 >= value) {
+			value = 0;
+		}
+
+		if (value >= el.childElementCount) {
+			value = el.childElementCount - 1;
+		}
+
+		const to = {scrollLeft: children[value].offsetLeft};
+		if (from.scrollLeft === to.scrollLeft) {
+			return;
+		}
+
+		// @TODO: Use animation api with from/to objects...
+	}
+
+	get items() {
+		return [...this.el.children];
+	}
+
+	destroy() {
+		const {el} = this;
+		const {classList} = el;
+
+		// Remove created id if it was created by carousel:
+		ID_MATCH.test(el.id) && el.removeAttribute('id');
+
+		// Remove scrollbar classes:
+		classList.remove(CLASS_VISIBLE_SCROLLBAR);
+		classList.remove(CLASS_INVISIBLE_SCROLLBAR);
+
+		// Remove buttons:
+		this._removeButtons();
+
+		// Remove pagination:
+		this._removePagination();
+	}
+
+	_update() {
+		const {height} = scrollbar.dimensions;
+		const hasInvisbleScrollbar = (height === 0);
+
+		if (hasInvisbleScrollbar === this._hasInvisbleScrollbar) {
+			return;
+		}
+
+		const classList = this.el.classList;
+		classList.add(hasInvisbleScrollbar ? CLASS_VISIBLE_SCROLLBAR : CLASS_INVISIBLE_SCROLLBAR);
+		classList.remove(hasInvisbleScrollbar ? CLASS_INVISIBLE_SCROLLBAR : CLASS_VISIBLE_SCROLLBAR);
+		this._hasInvisbleScrollbar = hasInvisbleScrollbar;
+	}
+
+	_addButtons() {
+		const {el, id, _options} = this;
+		if (!_options.hasButtons) {
+			return;
+		}
+
+		const {buttonTemplate, buttonClassName, buttonPrevious, buttonNext} = _options;
+
+		// Create previous buttons:
+		const [previous, next] = [buttonPrevious, buttonNext].map((data) =>
+			__render(buttonTemplate, {
+				...data,
+				controls: id,
+				className: `${buttonClassName} ${data.className}`
+			}));
+
+		this._previous = previous;
+		el.parentNode.appendChild(previous);
+
+		this._next = next;
+		el.parentNode.appendChild(next);
+	}
+
+	_removeButtons() {
+		const {_previous, _next} = this;
+		_previous && _previous.parentNode.removeChild(_previous);
+		_next && _next.parentNode.removeChild(_next);
+	}
+
+	_addPagination() {
+		const {el, id, items, _options} = this;
+		if (!_options.hasPagination) {
+			return;
+		}
+
+		const {paginationTemplate, paginationClassName, paginationLabel, paginationTitle} = _options;
+		const pagination = __render(paginationTemplate, {
+			items,
+			controls: id,
+			className: paginationClassName,
+			label: paginationLabel,
+			title: paginationTitle,
+		});
+
+		this._pagination = pagination;
+		el.parentNode.appendChild(pagination);
+	}
+
+	_removePagination() {
+		const {_pagination} = this;
+		_pagination && _pagination.parentNode.removeChild(_pagination);
+	}
+
+}
+
+export { Carousel };
---
+++ dist/caroucssel.umd.js
@@ -0,0 +1,388 @@
+(function (global, factory) {
+  if (typeof define === "function" && define.amd) {
+    define("caroucssel", ["exports"], factory);
+  } else if (typeof exports !== "undefined") {
+    factory(exports);
+  } else {
+    var mod = {
+      exports: {}
+    };
+    factory(mod.exports);
+    global.caroucssel = mod.exports;
+  }
+})(this, function (_exports) {
+  "use strict";
+
+  Object.defineProperty(_exports, "__esModule", {
+    value: true
+  });
+  _exports.Carousel = void 0;
+
+  function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _nonIterableSpread(); }
+
+  function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance"); }
+
+  function _iterableToArray(iter) { if (Symbol.iterator in Object(iter) || Object.prototype.toString.call(iter) === "[object Arguments]") return Array.from(iter); }
+
+  function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } }
+
+  function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest(); }
+
+  function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
+
+  function _iterableToArrayLimit(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+  function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
+  function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; var ownKeys = Object.keys(source); if (typeof Object.getOwnPropertySymbols === 'function') { ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) { return Object.getOwnPropertyDescriptor(source, sym).enumerable; })); } ownKeys.forEach(function (key) { _defineProperty(target, key, source[key]); }); } return target; }
+
+  function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+  function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+  function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+  function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+  function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+  var Scrollbar =
+  /*#__PURE__*/
+  function () {
+    function Scrollbar() {
+      var _this = this;
+
+      _classCallCheck(this, Scrollbar);
+
+      this._d = null;
+      window.addEventListener('resize', function () {
+        return _this._d = null;
+      });
+    }
+
+    _createClass(Scrollbar, [{
+      key: "dimensions",
+      get: function get() {
+        this._d = this._d || function () {
+          var inner = document.createElement('div'),
+              outer = document.createElement('div');
+          var width, w1, w2, height, h1, h2;
+          document.body.appendChild(outer);
+          outer.style.position = 'absolute';
+          outer.style.top = '0px';
+          outer.style.left = '0px';
+          outer.style.visibility = 'hidden';
+          outer.appendChild(inner); // Calculate width:
+
+          inner.style.width = '100%';
+          inner.style.height = '200px';
+          outer.style.width = '200px';
+          outer.style.height = '150px';
+          outer.style.overflow = 'hidden';
+          w1 = inner.offsetWidth;
+          outer.style.overflow = 'scroll';
+          w2 = inner.offsetWidth;
+          w2 = w1 === w2 ? outer.clientWidth : w2;
+          width = w1 - w2; // Calculate height:
+
+          inner.style.width = '200px';
+          inner.style.height = '100%';
+          outer.style.width = '150px';
+          outer.style.height = '200px';
+          outer.style.overflow = 'hidden';
+          h1 = inner.offsetHeight;
+          outer.style.overflow = 'scroll';
+          h2 = inner.offsetHeight;
+          h2 = h1 === h2 ? outer.clientHeight : h2;
+          height = h1 - h2;
+          document.body.removeChild(outer);
+          return {
+            width: width,
+            height: height
+          };
+        }();
+
+        return this._d;
+      }
+    }]);
+
+    return Scrollbar;
+  }();
+
+  function __render(template, data) {
+    var el = document.createElement('div');
+    el.innerHTML = template(data);
+    return el.firstChild;
+  }
+
+  function __templateButton(_ref) {
+    var className = _ref.className,
+        controls = _ref.controls,
+        label = _ref.label,
+        title = _ref.title;
+    return "<button class=\"".concat(className, "\" aria-label=\"").concat(label, "\" title=\"").concat(title, "\" aria-controls=\"").concat(controls, "\">\n\t\t<span>").concat(label, "</span>\n\t</button>");
+  }
+
+  function __templatePagination(_ref2) {
+    var className = _ref2.className,
+        controls = _ref2.controls,
+        items = _ref2.items,
+        label = _ref2.label,
+        title = _ref2.title;
+    return "<ul class=\"".concat(className, "\">\n\t\t").concat(items.map(function (item, index) {
+      var data = {
+        index: index,
+        item: item,
+        items: items
+      };
+      var labelStr = label(data);
+      var titleStr = title(data);
+      return "<li>\n\t\t\t\t<button aria-controls=\"".concat(controls, "\" aria-label=\"").concat(titleStr, "\" title=\"").concat(titleStr, "\">\n\t\t\t\t\t<span>").concat(labelStr, "</span>\n\t\t\t\t</button>\n\t\t\t</li>");
+    }).join(''), "\n\t</ul>");
+  }
+
+  var ID_NAME = function ID_NAME(count) {
+    return "caroucssel-".concat(count);
+  },
+      ID_MATCH = /^caroucssel-[0-9]*$/,
+      CLASS_VISIBLE_SCROLLBAR = 'has-visible-scrollbar',
+      CLASS_INVISIBLE_SCROLLBAR = 'has-invisible-scrollbar',
+      DEFAULTS = {
+    // Buttons:
+    hasButtons: false,
+    buttonClassName: 'button',
+    buttonTemplate: __templateButton,
+    buttonPrevious: null,
+    buttonNext: null,
+    // Pagination:
+    hasPagination: false,
+    paginationClassName: 'pagination',
+    paginationLabel: function paginationLabel(_ref3) {
+      var index = _ref3.index;
+      return "".concat(index + 1);
+    },
+    paginationTitle: function paginationTitle(_ref4) {
+      var index = _ref4.index;
+      return "Go to ".concat(index + 1, ". item");
+    },
+    paginationTemplate: __templatePagination
+  },
+      DEFAULTS_BUTTON_PREVIOUS = {
+    className: 'is-previous',
+    label: 'Previous',
+    title: 'Go to previous'
+  },
+      DEFAULTS_BUTTON_NEXT = {
+    className: 'is-next',
+    label: 'Next',
+    title: 'Go to next'
+  };
+
+  var carouselCount = 0,
+      scrollbar = null;
+
+  var Carousel =
+  /*#__PURE__*/
+  function () {
+    function Carousel(el) {
+      var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+
+      _classCallCheck(this, Carousel);
+
+      if (!el || !(el instanceof HTMLElement)) {
+        throw new Error("Carousel needs a dom element but \"".concat(_typeof(el), "\" was passed."));
+      }
+
+      this._el = el; // Create a singleton instance of scrollbar for all carousel instances:
+
+      scrollbar = scrollbar || new Scrollbar(); // Count all created instances to create unique id, if given dom element
+      // has no id-attribute:
+
+      carouselCount++;
+      el.id = el.id || ID_NAME(carouselCount);
+      this._id = el.id; // "deep" extend options and defaults:
+
+      this._options = _objectSpread({}, DEFAULTS, options);
+      this._options.buttonPrevious = _objectSpread({}, DEFAULTS_BUTTON_PREVIOUS, options.buttonPrevious);
+      this._options.buttonNext = _objectSpread({}, DEFAULTS_BUTTON_NEXT, options.buttonNext); // Render
+
+      this._update();
+
+      this._addButtons();
+
+      this._addPagination();
+    }
+
+    _createClass(Carousel, [{
+      key: "destroy",
+      value: function destroy() {
+        var el = this.el;
+        var classList = el.classList; // Remove created id if it was created by carousel:
+
+        ID_MATCH.test(el.id) && el.removeAttribute('id'); // Remove scrollbar classes:
+
+        classList.remove(CLASS_VISIBLE_SCROLLBAR);
+        classList.remove(CLASS_INVISIBLE_SCROLLBAR); // Remove buttons:
+
+        this._removeButtons(); // Remove pagination:
+
+
+        this._removePagination();
+      }
+    }, {
+      key: "_update",
+      value: function _update() {
+        var height = scrollbar.dimensions.height;
+        var hasInvisbleScrollbar = height === 0;
+
+        if (hasInvisbleScrollbar === this._hasInvisbleScrollbar) {
+          return;
+        }
+
+        var classList = this.el.classList;
+        classList.add(hasInvisbleScrollbar ? CLASS_VISIBLE_SCROLLBAR : CLASS_INVISIBLE_SCROLLBAR);
+        classList.remove(hasInvisbleScrollbar ? CLASS_INVISIBLE_SCROLLBAR : CLASS_VISIBLE_SCROLLBAR);
+        this._hasInvisbleScrollbar = hasInvisbleScrollbar;
+      }
+    }, {
+      key: "_addButtons",
+      value: function _addButtons() {
+        var el = this.el,
+            id = this.id,
+            _options = this._options;
+
+        if (!_options.hasButtons) {
+          return;
+        }
+
+        var buttonTemplate = _options.buttonTemplate,
+            buttonClassName = _options.buttonClassName,
+            buttonPrevious = _options.buttonPrevious,
+            buttonNext = _options.buttonNext; // Create previous buttons:
+
+        var _map = [buttonPrevious, buttonNext].map(function (data) {
+          return __render(buttonTemplate, _objectSpread({}, data, {
+            controls: id,
+            className: "".concat(buttonClassName, " ").concat(data.className)
+          }));
+        }),
+            _map2 = _slicedToArray(_map, 2),
+            previous = _map2[0],
+            next = _map2[1];
+
+        this._previous = previous;
+        el.parentNode.appendChild(previous);
+        this._next = next;
+        el.parentNode.appendChild(next);
+      }
+    }, {
+      key: "_removeButtons",
+      value: function _removeButtons() {
+        var _previous = this._previous,
+            _next = this._next;
+        _previous && _previous.parentNode.removeChild(_previous);
+        _next && _next.parentNode.removeChild(_next);
+      }
+    }, {
+      key: "_addPagination",
+      value: function _addPagination() {
+        var el = this.el,
+            id = this.id,
+            items = this.items,
+            _options = this._options;
+
+        if (!_options.hasPagination) {
+          return;
+        }
+
+        var paginationTemplate = _options.paginationTemplate,
+            paginationClassName = _options.paginationClassName,
+            paginationLabel = _options.paginationLabel,
+            paginationTitle = _options.paginationTitle;
+
+        var pagination = __render(paginationTemplate, {
+          items: items,
+          controls: id,
+          className: paginationClassName,
+          label: paginationLabel,
+          title: paginationTitle
+        });
+
+        this._pagination = pagination;
+        el.parentNode.appendChild(pagination);
+      }
+    }, {
+      key: "_removePagination",
+      value: function _removePagination() {
+        var _pagination = this._pagination;
+        _pagination && _pagination.parentNode.removeChild(_pagination);
+      }
+    }, {
+      key: "el",
+      get: function get() {
+        return this._el;
+      }
+    }, {
+      key: "id",
+      get: function get() {
+        return this._id;
+      }
+    }, {
+      key: "index",
+      get: function get() {
+        var el = this.el;
+        var children = el.children,
+            clientWidth = el.clientWidth;
+        var length = children.length;
+        var outerLeft = el.getClientRects()[0].left;
+        var offset = clientWidth / 2;
+        var index = 0,
+            left;
+
+        for (; index < length; index++) {
+          left = el.children[index].getClientRects()[0].left - outerLeft + offset;
+
+          if (left >= 0 && left < clientWidth) {
+            return index;
+          }
+        }
+
+        return index - 1;
+      },
+      set: function set(value) {
+        var el = this.el;
+        var children = el.children,
+            scrollLeft = el.scrollLeft;
+        var from = {
+          scrollLeft: scrollLeft
+        };
+
+        if (-1 >= value) {
+          value = 0;
+        }
+
+        if (value >= el.childElementCount) {
+          value = el.childElementCount - 1;
+        }
+
+        var to = {
+          scrollLeft: children[value].offsetLeft
+        };
+
+        if (from.scrollLeft === to.scrollLeft) {
+          return;
+        } // @TODO: Use animation api with from/to objects...
+
+      }
+    }, {
+      key: "items",
+      get: function get() {
+        return _toConsumableArray(this.el.children);
+      }
+    }]);
+
+    return Carousel;
+  }();
+
+  _exports.Carousel = Carousel;
+});
