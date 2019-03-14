0d3b5bc3dd27b52b9357cf060f71a520088aa292
Norman Rusch <norman.rusch@moccu.com>
2019-03-14 18:26:36 +0100

    Update ghpages

--- web/script.js
+++ web/script.js
@@ -1,8 +1,15 @@
 (function () {
 	var element = document.querySelector('.caroucssel');
+	var items = document.querySelectorAll('.item');
 
 	new window.caroucssel.Carousel(element, {
 		hasButtons: true,
-		hasPagination: true
+		hasPagination: true,
+		onScroll: function(event) {
+			console.log(items);
+			items.forEach(function(item, index) {
+				item.classList[event.index === index ? 'add' : 'remove']('is-active');
+			});
+		}
 	});
 })();
