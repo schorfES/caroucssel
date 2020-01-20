dca444be68513685f2e5a123506091526839635e
Norman Rusch <n.rusch@edenspiekermann.com>
2020-01-20 14:53:42 +0100

    Build

---
+++ dist/caroucssel.d.ts
@@ -0,0 +1,86 @@
+export type ButtonParams = {
+    controls: string;
+    className: string;
+    label: string;
+    title: string;
+};
+
+export type ButtonTemplate = (params: ButtonParams) => string;
+
+export type ButtonOptions = {
+    className?: string;
+    label?: string;
+    title?: string;
+};
+
+export type PaginationTextParams = {
+    index: number;
+    item: Element;
+    items: Element[];
+};
+
+export type PaginationText = (params: PaginationTextParams) => string;
+
+export type PaginationParams = {
+    controls: string;
+    className: string;
+    label: PaginationText;
+    title: PaginationText;
+    items: Element[];
+};
+
+export type PaginationTemplate = (params: PaginationParams) => string;
+
+export type PaginationLabelTemplate = PaginationText;
+
+export type PaginationTitleTemplate = PaginationText;
+
+export type ScrollHook = (event: {
+	index: number[];
+	type: 'scroll';
+	target: Carousel;
+	originalEvent: Event;
+}) => any;
+
+export type Options = {
+    // Buttons:
+    hasButtons?: boolean;
+    buttonClassName?: string;
+    buttonTemplate?: ButtonTemplate;
+    buttonPrevious?: ButtonOptions;
+    buttonNext?: ButtonOptions;
+
+    // Pagination:
+    hasPagination?: boolean;
+    paginationClassName?: string;
+    paginationTemplate?: PaginationTemplate;
+    paginationLabel?: PaginationLabelTemplate;
+    paginationTitle?: PaginationTitleTemplate;
+
+    // Scrollbars:
+    hasScrollbars?: boolean;
+
+    // Hooks:
+    onScroll?: ScrollHook;
+};
+
+export class Carousel {
+	constructor(el: Element, options: Options);
+	public get el(): Element;
+	public get id(): string;
+	public get index(): number[];
+	public set index(values: number[]);
+	public get items(): Element[];
+	public destroy(): void;
+	public update(): void;
+
+	protected _updateScrollbars(): void;
+	protected _addButtons(): void;
+	protected _updateButtons(index: number[]): void;
+	protected _removeButtons(): void;
+	protected _addPagination(): void;
+	protected _updatePagination(index: number[]): void;
+	protected _removePagination(): void;
+	protected _onScroll(event: Event): void;
+	protected _onResize(event: Event): void;
+}
--- dist/caroucssel.umd.js
+++ dist/caroucssel.umd.js
@@ -10,7 +10,7 @@
     factory(mod.exports);
     global.caroucssel = mod.exports;
   }
-})(this, function (_exports) {
+})(typeof globalThis !== "undefined" ? globalThis : typeof self !== "undefined" ? self : this, function (_exports) {
   "use strict";
 
   Object.defineProperty(_exports, "__esModule", {
@@ -22,7 +22,7 @@
 
   function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
 
-  function _iterableToArrayLimit(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+  function _iterableToArrayLimit(arr, i) { if (!(Symbol.iterator in Object(arr) || Object.prototype.toString.call(arr) === "[object Arguments]")) { return; } var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
 
   function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 
@@ -34,7 +34,9 @@
 
   function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } }
 
-  function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; var ownKeys = Object.keys(source); if (typeof Object.getOwnPropertySymbols === 'function') { ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) { return Object.getOwnPropertyDescriptor(source, sym).enumerable; })); } ownKeys.forEach(function (key) { _defineProperty(target, key, source[key]); }); } return target; }
+  function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+  function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
 
   function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
@@ -224,9 +226,9 @@
       el.id = el.id || ID_NAME(instanceCount);
       this._id = el.id; // "deep" extend options and defaults:
 
-      this._options = _objectSpread({}, DEFAULTS, options);
-      this._options.buttonPrevious = _objectSpread({}, DEFAULTS_BUTTON_PREVIOUS, options.buttonPrevious);
-      this._options.buttonNext = _objectSpread({}, DEFAULTS_BUTTON_NEXT, options.buttonNext);
+      this._options = _objectSpread({}, DEFAULTS, {}, options);
+      this._options.buttonPrevious = _objectSpread({}, DEFAULTS_BUTTON_PREVIOUS, {}, options.buttonPrevious);
+      this._options.buttonNext = _objectSpread({}, DEFAULTS_BUTTON_NEXT, {}, options.buttonNext);
       this._items = _toConsumableArray(this.el.children); // Render:
 
       this._addButtons();
