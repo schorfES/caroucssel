25777cad86c3cfdaac436f8b4c318cf6fbf27ccf
Norman Rusch <webmaster@grind-it.de>
2020-07-01 10:00:16 +0200

    Add filter for items
    This ignores invisible elements as items and allows to filter items manually.

--- src/index.js
+++ src/index.js
@@ -36,6 +36,8 @@ const
 	ID_NAME = (count) => `caroucssel-${count}`,
 	ID_MATCH = /^caroucssel-[0-9]*$/,
 
+	INVISIBLE_ELEMENTS = /^(link|meta|noscript|script|style|title)$/i,
+
 	EVENT_SCROLL = 'scroll',
 	EVENT_RESIZE = 'resize',
 
@@ -57,6 +59,9 @@ const
 		// Scrollbars, set to true when use default scrolling behaviour
 		hasScrollbars: false,
 		scrollbarsMaskClassName: 'caroucssel-mask',
+
+		// filter
+		filterItem: () => true,
 
 		// Hooks:
 		onScroll: null
@@ -103,7 +108,8 @@ export class Carousel {
 		this._options.buttonPrevious = {...DEFAULTS_BUTTON_PREVIOUS, ...options.buttonPrevious};
 		this._options.buttonNext = {...DEFAULTS_BUTTON_NEXT, ...options.buttonNext};
 
-		this._items = [...this.el.children];
+		// Receive all items:
+		this._updateItems();
 
 		// Render:
 		this._addButtons();
@@ -214,10 +220,18 @@ export class Carousel {
 
 	update() {
 		const {index} = this;
-		this._items = [...this.el.children];
+		this._updateItems();
 		this._updateButtons(index);
 		this._updatePagination(index);
 		this._updateScrollbars();
+	}
+
+	_updateItems() {
+		const { el, _options } = this;
+		this._items = Array
+			.from(el.children)
+			.filter((item) => !INVISIBLE_ELEMENTS.test(item.tagName) && !item.hidden)
+			.filter(_options.filterItem);
 	}
 
 	_updateScrollbars() {
