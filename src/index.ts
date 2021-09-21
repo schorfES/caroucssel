42111d7d1a071b7592f8d43eb06c43aac45b7e09
Norman Rusch <webmaster@grind-it.de>
2021-09-21 03:16:23 +0200

    Expose plugins for UMD build

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
+import { Buttons } from './plugins/buttons';
+import { Mask } from './plugins/mask';
+import { Pagination } from './plugins/pagination';
+
+
 export { Carousel } from './caroucssel';
+export const plugins = { Buttons, Mask, Pagination };
