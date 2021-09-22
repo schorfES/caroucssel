16df684cba2a5bf263c3f780159e4b9a8d88638b
Norman Rusch <webmaster@grind-it.de>
2021-09-22 18:07:21 +0200

    Add version constant in post build step

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,7 @@ export { Mask } from './features/mask';
 export { Mouse } from './features/mouse';
 export { Pagination } from './features/pagination';
 export { Carousel } from './carousel';
+
+// This will be replaced by the version from the package.json
+// See: scripts/build_post.js
+export const version = '__VERSION__';
