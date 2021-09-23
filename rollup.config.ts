b12af41556e63c795d5221f26ca8c1c0b2ee558e
Norman Rusch <webmaster@grind-it.de>
2021-09-23 17:41:38 +0200

    Add IIFE dist format

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,94 +1,132 @@
 import path from 'path';
 
-import {
-	getBabelOutputPlugin as babel,
-	RollupBabelOutputPluginOptions as BabelOutputPluginOptions,
-} from '@rollup/plugin-babel';
-import replace, { RollupReplaceOptions as ReplaceInputPluginOptions } from '@rollup/plugin-replace';
+import { getBabelOutputPlugin as babel, RollupBabelOutputPluginOptions as BabelOutputPluginOptions } from '@rollup/plugin-babel';
+import replace from '@rollup/plugin-replace';
 import typescript from '@rollup/plugin-typescript';
-import type * as rollup from 'rollup';
-import cleanup, { Options as CleanupInputPluginOptions } from 'rollup-plugin-cleanup';
+import cleanup from 'rollup-plugin-cleanup';
 import { terser, Options as TerserInputPluginOptions } from 'rollup-plugin-terser';
 
 
-const MODULE_INPUT = path.join(__dirname, 'src/index.ts');
 const MODULE_NAME = 'caroucssel';
 
-const PLUGIN_SETTINGS_REPLACE: ReplaceInputPluginOptions = {
-	extensions: ['.js', '.ts'],
-	preventAssignment: true,
-	values: {
-		'process.env.NODE_ENV': JSON.stringify('production'),
-	},
-};
-
-const PLUGIN_SETTINGS_CLEANUP: CleanupInputPluginOptions = {
-	extensions: ['.js', '.ts'],
-	comments: 'none',
-};
-
-const PLUGIN_SETTINGS_BABEL: BabelOutputPluginOptions = {
+const PLUGIN_OPTIONS_BABEL_UMD: BabelOutputPluginOptions = {
 	allowAllFormats: true,
 	moduleId: MODULE_NAME,
 	presets: [
 		['@babel/preset-env', { modules: false }],
 	],
-	plugins: ['@babel/plugin-transform-modules-umd'],
+	plugins: [
+		['@babel/plugin-transform-modules-umd', {
+			globals: { [MODULE_NAME]: MODULE_NAME },
+			exactGlobals: true,
+		}],
+	],
 };
 
-const PLUGIN_SETTINGS_TERSER: TerserInputPluginOptions = {
+const PLUGIN_OPTIONS_TERSER: TerserInputPluginOptions = {
 	compress: true,
 	mangle: true,
 };
 
-function createBundle(output: rollup.OutputOptions = {}, plugins: rollup.Plugin[] = []) {
-	return {
-		input: MODULE_INPUT,
-		output: {
-			format: 'es',
-			name: MODULE_NAME,
-			...output,
-		},
-		plugins: [
-			typescript(),
-			replace(PLUGIN_SETTINGS_REPLACE),
-			cleanup(PLUGIN_SETTINGS_CLEANUP),
-			...plugins,
+const TYPES = [
+	// CJS Builds:
+	// ---------------------------------------------------------------------------
+	{
+		format: 'cjs',
+		dir: path.join(__dirname, 'dist/formats/cjs/'),
+		plugins: [],
+		entries: [
+			{
+				name: 'caroucssel',
+				input: path.join(__dirname, 'src/index.ts'),
+				plugins: [],
+			},
+			{
+				name: 'caroucssel.min',
+				input: path.join(__dirname, 'src/index.ts'),
+				plugins: [terser(PLUGIN_OPTIONS_TERSER)],
+			},
 		],
-	};
-}
+	},
 
-export default [
-	createBundle(
-		{
-			file: path.join(__dirname, 'dist/formats/cjs/caroucssel.js'),
-			format: 'cjs',
-		},
-	),
-	createBundle(
-		{
-			file: path.join(__dirname, 'dist/formats/cjs/caroucssel.min.js'),
-			format: 'cjs',
-		},
-		[
-			terser(PLUGIN_SETTINGS_TERSER),
+	// UMD Builds:
+	// ---------------------------------------------------------------------------
+	{
+		format: 'es',
+		dir: path.join(__dirname, 'dist/formats/umd/'),
+		plugins: [babel(PLUGIN_OPTIONS_BABEL_UMD)],
+		entries: [
+			{
+				name: 'caroucssel',
+				input: path.join(__dirname, 'src/index.ts'),
+				plugins: [],
+			},
+			{
+				name: 'caroucssel.min',
+				input: path.join(__dirname, 'src/index.ts'),
+				plugins: [terser(PLUGIN_OPTIONS_TERSER)],
+			},
 		],
-	),
-	createBundle(
-		{
-			file: path.join(__dirname, 'dist/formats/umd/caroucssel.js'),
-		},
-		[
-			babel(PLUGIN_SETTINGS_BABEL),
+	},
+
+	// IIFE Builds:
+	// ---------------------------------------------------------------------------
+	{
+		format: 'iife',
+		dir: path.join(__dirname, 'dist/formats/iife/'),
+		plugins: [],
+		entries: [
+			{
+				name: 'caroucssel',
+				input: path.join(__dirname, 'src/index.ts'),
+				plugins: [],
+			},
+			{
+				name: 'caroucssel.min',
+				input: path.join(__dirname, 'src/index.ts'),
+				plugins: [terser(PLUGIN_OPTIONS_TERSER)],
+			},
 		],
-	),
-	createBundle(
-		{
-			file: path.join(__dirname, 'dist/formats/umd/caroucssel.min.js'),
-		},
-		[
-			babel(PLUGIN_SETTINGS_BABEL),
-			terser(PLUGIN_SETTINGS_TERSER),
-		],
-	),
+	},
 ];
+
+
+export default TYPES.flatMap((type) => {
+	const { format, dir, entries } = type;
+
+	return entries.map((entry) => {
+		const { input, name } = entry;
+		const file = path.join(dir, `${name}.js`);
+
+		return {
+			input,
+			output: {
+				name: MODULE_NAME,
+				file,
+				format,
+			},
+			plugins: [
+				// Typescript plugin
+				typescript(),
+
+				// Replace plugin: update NODE_ENV
+				replace({
+					extensions: ['.js', '.ts'],
+					preventAssignment: true,
+					values: {
+						'process.env.NODE_ENV': JSON.stringify('production'),
+					},
+				}),
+
+				// Cleanup plugin: remove all comments
+				cleanup({
+					extensions: ['.js', '.ts'],
+					comments: 'none',
+				}),
+
+				...type.plugins,
+				...entry.plugins,
+			],
+		};
+	});
+});
