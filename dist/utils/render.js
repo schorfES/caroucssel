e074dd573e0d24d1fed5f527997b62064c787425
Norman Rusch <webmaster@grind-it.de>
2021-09-28 16:34:27 +0200

    0.12.0-4

--- dist/utils/render.js
+++ dist/utils/render.js
@@ -2,6 +2,8 @@
  * Renders a template (function) to into an HTML element. The template needs to
  * return a valid HTML string (with a single root element). A context with
  * key/value pairs will be passed into the template to allow dynamic rendering.
+ * @typeParam El is the type of the rendered dom element.
+ * @typeParam Context is the shape of the context data object
  * @param template a function to render a template
  * @param context context data for the template
  * @returns the rendered htmml element
