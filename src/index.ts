9944971344abff39215252bed3f57bdc5d65bb57
Norman Rusch <webmaster@grind-it.de>
2021-09-29 19:08:24 +0200

    Export types directly

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ export { Buttons } from './features/buttons';
 export { Pagination } from './features/pagination';
 export { Carousel, Mask } from './carousel';
 
+export * from './types';
+
 // This will be replaced by the version from the package.json
 // See: scripts/build_post.js
 export const version = '__VERSION__';
